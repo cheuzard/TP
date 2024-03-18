6a6e37e0bc3da64d75756ad0705ce3adadae4864
hichem <hichem172019@gmail.com>
2024-03-18 12:51:22 +0100

    adding Tableau1.c

---
+++ Tableau1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int tab[12];
+    printf("inserez les élements du tableau ");
+    for (int i = 0; i < 12; i++)
+    {
+        int v;
+        do{
+            scanf("%d", &v);
+        }while (v<1 || v>12);
+        tab[i]=v;
+    }
+    for (int i = 0; i < 12; i++)
+    {
+        printf("|&d|", tab[i]);
+    }
+    
+    
+    return 0;
+}
+/*todo
+meme tableau en supprimant la redondonce au lieux de filtrer les chiffre entre 12 et 1
+*/
